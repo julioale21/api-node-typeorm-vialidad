812cdca41e70f7b04fced756017de65bf8639200
julioale21 <30907777+julioale21@users.noreply.github.com>
2021-10-19 11:08:32 -0300

    added database config

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import { createConnection } from "typeorm";
 
 import userRouter from "./routes/user.routes";
 
