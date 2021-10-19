5140638fc957d5550b9e133d8a2ca1154b009c14
julioale21 <30907777+julioale21@users.noreply.github.com>
2021-10-19 10:53:20 -0300

    added user.router

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,17 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 
+import userRouter from "./routes/user.routes";
+
 const app = express();
 
 //middleware
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
+
+// routes
+app.use(userRouter);
+
 
 app.listen(3000)
